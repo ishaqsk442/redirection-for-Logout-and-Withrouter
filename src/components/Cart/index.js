8cf781e1558ded2bacbe29646ee1dc89551fdf9c
ishaqsk442 <skishaq456@gmail.com>
2023-07-17 16:59:45 +0000

    logout redirection and using of WithRouter in header to use history

---
+++ src/components/Cart/index.js
@@ -0,0 +1,13 @@
+// Write your JS code here
+// Write your JS code here
+const Cart = () => (
+  <div>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+      alt="cart"
+    />
+    <p>Products</p>
+  </div>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,12 @@
+// Write your JS code here
+const Products = () => (
+  <div>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+      alt="products"
+    />
+    <p>Products</p>
+  </div>
+)
+
+export default Products
